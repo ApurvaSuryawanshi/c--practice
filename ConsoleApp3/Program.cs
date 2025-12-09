a855704a6df6c81957c29032d7fb5d8faaa5fa20
Apurva Suryawanshi <Apurva Suryawanshi@DESKTOP-NLSHS0M>
2025-12-09 12:15:16 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void MyMethod(string country)
+    {
+        Console.WriteLine(country);
+    }
+    static void Main(string[] args)
+    {
+        MyMethod("Sweden");
+        Console.ReadLine();
+    }
+
+    
+}
