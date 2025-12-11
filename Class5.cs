4991b8eed7d03b13e99e964bfe514f9c2553eb9f
Suryawanshi <suryawanshi91apurva@gmail.com>
2025-12-11 17:37:59 +0530

    change

--- Class5.cs
+++ Class5.cs
@@ -6,10 +6,12 @@ class Prog1
 		int sum2 = sum1 + 250;
 		int sum3 = sum2 + sum2;
 		int sum4 = sum3 + sum3;
+		int sum5 = sum1;
 		Console.WriteLine(sum1);
 		Console.WriteLine(sum2);
 		Console.WriteLine(sum3);
 		Console.WriteLine(sum4);
+		Console.WriteLine(sum5);
 	}
 
 }
