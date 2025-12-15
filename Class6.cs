e45186252388c7fd55f8ba33b6532c7630e63af8
Suryawanshi <suryawanshi91apurva@gmail.com>
2025-12-15 17:15:24 +0530

    oop program

---
+++ Class6.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Class6.cs
+	{
+	 string color = "red";
+     static void Main(string[] args) {
+	   Class6 myObj = new Class6();
+	Console.WriteLine(myObj.color);
+      }
+	}
