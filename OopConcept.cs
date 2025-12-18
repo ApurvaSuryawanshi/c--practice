ede4db3a0ab412a9d63238ed9255cf3d6d500664
Suryawanshi <suryawanshi91apurva@gmail.com>
2025-12-18 11:15:55 +0530

    oops concept

---
+++ OopConcept.cs
@@ -0,0 +1,25 @@
+using System;
+class Class1
+{
+    string color = "red";
+
+    void MyMethod()
+    {
+        Console.WriteLine("My car color is white");
+    }
+
+    static void Main(string[] args)
+    {
+        Class1 myObj = new Class1();
+        Class1 myObj2 = new Class1();
+
+
+        Console.WriteLine(myObj2.color);
+
+        Console.WriteLine(myObj.color);
+
+        myObj.MyMethod();
+
+    }
+}
+
